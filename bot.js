8bc053c68071f82b9f41465167767aa90774edd7
elsmeri2 <51977392+elsmeri2@users.noreply.github.com>
2019-06-21 18:58:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -208,7 +208,7 @@ client.on('message',async message => {
     
 if(message.author.bot) return;
 if(!credits[message.author.id]) credits[message.author.id] = {
-    credits: 99999999999999999999999
+    credits: 9999999999
 };
 
 let userData = credits[message.author.id];
@@ -227,7 +227,7 @@ message.channel.send(`**${message.author.username}, your :credit_card: balance i
 });
 
 client.on('message', async message => {
-    let amount = 250;
+    let amount = 9999999999;
     if(message.content.startsWith(prefix + "daily")) {
     if(message.author.bot) return;
     if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
