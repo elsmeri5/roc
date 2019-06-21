a2a8ff99ef9cc5a27f05f8536f9b6a99c844e591
elsmeri2 <51977392+elsmeri2@users.noreply.github.com>
2019-06-21 22:56:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['Bad One 🖕🏽'];
+    var setGame = ['Bad One 🖕🏽','Bad One 🖕'];
     var i = -1;
     var j = 0;
     setInterval(function (){
